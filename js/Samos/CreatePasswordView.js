325472d4f6d72916f34ec939513394f20b6cca51
PanYibin <>
2018-06-29 02:39:25 +0800

    create password UI

--- js/Samos/CreatePasswordView.js
+++ js/Samos/CreatePasswordView.js
@@ -21,7 +21,8 @@ export default class CreatePasswordView extends Component {
         super(props);
         this.state = {
             pinCode: "",
-            pinCodeConfirm: ""
+            pinCodeConfirm: "",
+            pinCodeHint:""
         };
     }
 
@@ -53,34 +54,50 @@ export default class CreatePasswordView extends Component {
             <Modal visible={this.props.modalVisible} transparent={true}>
                 <View style={style.background}>
                     <View style={style.container}>
-                        <Text>Enter password</Text>
-                        <TextInput onChangeText={
+                    <Text style={style.title}>Wallet password</Text>
+                    <Text style={style.description}>Please create a password for your wallet, the password will be used for transaction and removing wallet.</Text>
+                        <Text style={style.passwordTitle}>Enter password(6 digits)</Text>
+                        <TextInput 
+                        style={style.passwordInput}
+                        secureTextEntry={true}
+                        onChangeText={
                             text => {
                                 this.setState({ pinCode: text });
                             }
                         }
+                        placeholder={'Please input password'}
                         ></TextInput>
-                        <Text>Confirm password</Text>
-                        <TextInput onChangeText={
+                        <View style={style.seperator}/>
+                        <Text style={style.passwordTitle}>Confirm password</Text>
+                        <TextInput
+                        style={style.passwordInput} 
+                        secureTextEntry={true}
+                        placeholder={'Please confirm password'}
+                        onChangeText={
                             text => {
                                 this.setState({ pinCodeConfirm: text });
                             }
                         }></TextInput>
-                        <TouchableOpacity onPress={
-                            () => {
-                                Alert.alert(this.state.pinCode);
+                        <View style={style.seperator}/>
+                        <Text style={style.passwordTitle}>Hint(optional)</Text>
+                        <TextInput 
+                        style={style.passwordInput}
+                        placeholder={'Please input hint'}
+                        onChangeText={
+                            text => {
+                                this.setState({ pinCodeHint: text });
                             }
-                        }>
-                            <Text>
-                                show password
-                                </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={
+                        }></TextInput>
+                        <View style={style.seperator}/>
+                        <Text style={style.passwordHintDescription}>We won't store your password, you can't find your password if you forget it. You can set a hint in case you forget your password.</Text>
+                        <TouchableOpacity 
+                        style={style.createButton}
+                        onPress={
                             () => {
                                 this.tapCreate();
                             }
                         }>
-                            <Text>
+                            <Text style={style.createButtonText}>
                                 create
                                 </Text>
                         </TouchableOpacity>
@@ -100,9 +117,64 @@ const style = StyleSheet.create(
             backgroundColor: 'rgba(0,0,0,0.5)'
         },
         container: {
-            width: 200,
-            height: 200,
-            backgroundColor: 'green'
+            width: 292,
+            height: 510,
+            backgroundColor: 'white'
+        },
+        //
+        title:{
+            marginTop:24,
+            fontSize:17,
+            fontWeight:'bold',
+            color:'#414042',
+            textAlign:'center'
+        },
+        description:{
+            fontSize:11,
+            color:'#6d6f71',
+            marginTop:18,
+            marginLeft:20,
+            marginRight:20
+        },
+        passwordTitle:{
+            marginTop:20,
+            marginLeft:20,
+            fontSize:13,
+            color:'#414042'
+        },
+        passwordInput:{
+            marginTop:20,
+            marginLeft:20
+        },
+        seperator:{
+            marginTop:14,
+            marginLeft:20,
+            marginRight:20,
+            height:0.5,
+            backgroundColor:'black'
+        },
+        passwordHintDescription:{
+            marginTop:18,
+            fontSize:11,
+            color:'#6d6f71',
+            marginLeft:20,
+            marginRight:20
+        },
+        createButton:{
+            marginTop:26,
+            marginLeft:20,
+            marginRight:20,
+            height:40,
+            borderColor:'#414042',
+            borderWidth:0.5,
+            justifyContent:'center'
+        },
+        createButtonText:{
+            fontSize:17,
+            fontWeight:'bold',
+            color:'#414042',
+            textAlign:'center'
         }
+
     }
 );
